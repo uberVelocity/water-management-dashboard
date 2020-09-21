0046d764e89ea929db72c7eb9ecbe0eb3d73cc72
ubervelocity <mihaitu.popescu@gmail.com>
2020-09-21 08:27:33 +0200

    Removes useless files

--- backend/routes/api/sensor.js
+++ backend/routes/api/sensor.js
@@ -1,22 +1,20 @@
 const router = require('express').Router();
 const { connect } = require('../../middleware/cass');
 
+// GET URL:PORT/api/sensor/
 router.get('/', connect, (req, res) => {
     const cassandraClient = res.client;
     const query = 'SELECT * FROM sensor';
-    cassandraClient.execute(query, function (err, res) {
+    cassandraClient.execute(query, function (err, result) {
         if (!err) {
             console.log('Good');
-            console.log(res);
+            console.log(result);
+            res.status(200).json({ result });
         } else {
             console.log(err);
+            res.status(500).send(err.message);
         }
     });
-    
 });
-
-
-
-
 
 module.exports = router;
--- backend/server/index.js
+++ backend/server/index.js
@@ -4,5 +4,20 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+// Setup bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
+// Setup cors and express
+app.use(cors());
+app.use(express.json());
 
+// Import routes
+const sensor = require('../routes/api/sensor');
+
+// Use routes
+app.use('/api/sensor', sensor);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Backend started on port ${port}`));
