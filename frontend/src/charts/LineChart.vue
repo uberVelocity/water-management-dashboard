d22ec3fcec7f617986b635a8318f7bbda63d7523
Radu Catarambol <catarambolradu@gmail.com>
2020-10-03 17:53:44 +0300

    Creating separate files for charts

---
+++ frontend/src/charts/LineChart.vue
@@ -0,0 +1,38 @@
+<template>
+    <VueApexCharts width="500" type="line" :options="options" :series="series"></VueApexCharts>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+    name: 'LineChart',
+    components: {
+        VueApexCharts
+    },
+    data() {
+      return {
+        series: [],
+        xaxis: {
+          type: 'datetime'
+        },
+        options: {
+          colors: ['#3e3ea3', '#333333', '#ac3a43'],
+          chart: {
+            id: 'vuechart-example',
+          },
+          dataLabels: {
+            enabled: false
+          },
+          noData: {
+            text: 'Loading...'
+          }
+        },
+      }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
