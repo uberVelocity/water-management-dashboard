867ded15c8a8327f6068e00d0d9f8605e53d4471
ubervelocity <mihaitu.popescu@gmail.com>
2020-10-22 20:20:27 +0200

    Adds mongodb replication by manually copying the config after initialization

--- mongo-master/mongo-init.js
+++ mongo-master/mongo-init.js
@@ -1,7 +1,10 @@
-db = (new Mongo('localhost:27017')).getDB('test')
 config = {
     "_id" : "rs0",
     "members" : [
+        {
+            "_id" : 0,
+            "host" : "mongo_1:27017"
+        },
         {
             "_id" : 1,
             "host" : "mongo_2:27017"
