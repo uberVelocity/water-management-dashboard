f5e8f815f8915f5a6efe7b99cb8875670c682367
ubervelocity <mihaitu.popescu@gmail.com>
2020-10-22 13:59:31 +0200

    Historical data in Kubernetes in graphs

--- backend/routes/api/sensor.js
+++ backend/routes/api/sensor.js
@@ -26,6 +26,7 @@ router.get('/', connectCassandra, async (req, res) => {
             } else {
                 console.log('Unsuccessful query!');
                 res.status(500).send(err.message);
+                return;
             }
             resolve();
         });
