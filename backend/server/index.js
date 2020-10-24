ae328ad1f6033fb9144810ce3d6518ca387f1b93
ubervelocity <mihaitu.popescu@gmail.com>
2020-10-24 16:54:58 +0200

    Adds socket live updates: subscribes on every client connection

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,7 +1,7 @@
 const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
-  clientId: 'my-app',
+  clientId: 'backend-live-socket',
   brokers: ['kafka:9092']
 })
 
@@ -40,23 +40,39 @@ app.get('/api', (req, res) => {
     });
 });
 
+async () => {
+
+}
+
 // Start listening for requests
 server.listen(port, () => console.log(`Backend started on port ${port}`));
 
-// ##########
-
 
 const io = require('socket.io')(server);
-const connections = [];
+let connections = [];
+
+// function pushToSocket(data) {
+//     for (connection of connections) {
+//         connection.emit("pressure", data)
+//     }
+// }
 
 // Listen for socket connections
-io.sockets.on('connection', async (socket) => {
+io.of('/socket.io/').on('connection', async (socket) => {
     // Push connection to client on connections stack
     connections.push(socket);
+
+    // Connect to Kafka
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'sensor_data', fromBeginning: true })
+
     console.log(`a user has connected: ${connections.length} connected`);
 
-    await consumer.connect()  
-    await consumer.subscribe({ topic: 'sensor_data', fromBeginning: true })
+    console.log('Emitting to pressure')
+    // io.sockets.emit("pressure", "test")
+    // socket.broadcast.emit("pressure", "test")
+    console.log('Emitted to pressure')
+    // socket.emit("pressure", "test2")
 
     await consumer.run({
         eachMessage: async ({topic, partition, message}) => {
@@ -71,10 +87,11 @@ io.sockets.on('connection', async (socket) => {
             console.log(`emitting to ${type}`)
 
             if (type == "pressure") {
-                console.log('emitting to pressure using socket.broadcast.emit()')
-                socket.broadcast.emit("pressure", data)
-                console.log('emitting to pressure using io.sockets.emit()')
-                io.sockets.emit("pressure", data)
+                // console.log('emitting to pressure using socket.broadcast.emit()')
+                // socket.broadcast.emit("pressure", data)
+                // console.log('emitting to pressure using io.sockets.emit()')
+                // io.sockets.emit("pressure", data)
+                socket.emit("pressure", data)
             } 
             else if (type == "leakage") {
                 socket.broadcast.emit("leakage", data)
